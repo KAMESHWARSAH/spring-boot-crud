2475d46e4f6f6e3e97ee021e698af872de8e4d13
kameshwar sah <56642875+kameshwar-web@users.noreply.github.com>
2023-08-07 12:33:57 +0530

    Full CRUD code

--- src/main/java/com/example/springboot/controller/EmployeeController.java
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -1,15 +1,22 @@
 package com.example.springboot.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.springboot.exception.ResorsseNotFoundException;
 import com.example.springboot.model.Employee;
 import com.example.springboot.repository.EmployeeRepository;
 
@@ -33,5 +40,36 @@ public class EmployeeController {
 		return employeeRepository.save(employee);
 	}
 	
+//	Get Employee By id Rest API
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee>  getEmployeeById(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(()-> new ResorsseNotFoundException("Employee Not exit with id:" + id));
+		return ResponseEntity.ok(employee);
+	}
+	// Update Employee Rest API
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails){
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(()-> new ResorsseNotFoundException("Employee Not exit with id:" + id));
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmail(employeeDetails.getEmail());
+		
+		Employee updatedEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok(updatedEmployee);
+	}
 	
+	// Delete Rest API
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity <Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(()-> new ResorsseNotFoundException("Employee Not exit with id:" + id));
+		employeeRepository.delete(employee);
+		Map<String, Boolean> response  =  new HashMap <>();
+		response.put("deleted", Boolean.TRUE);
+		return ResponseEntity.ok(response);
+	}
+
 }
+
